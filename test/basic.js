49da20e614beabd1a2d640d665d2c6926dfbd49d
clux <analsandblaster@gmail.com>
2013-03-05 23:51:51 +0000

    a basic test that works

--- test/basic.js
+++ test/basic.js
@@ -12,38 +12,41 @@ function Pumper(data) {
 inherits(Pumper, stream.Readable);
 
 Pumper.prototype._read = function (size) {
+  if (!this.data.length) {
+    this.push(null);
+    return null;
+  }
   var out = this.data.slice(0, size);
   this.push(out);
   this.data = this.data.slice(size);
+  return out.length;
 };
 
 // collect stream that will buffer on the other end
-function Collector(onEnd) {
+function Collector(onFinish) {
   stream.Writable.call(this);
   this.buf = '';
-  this.on('end', onEnd)
+  this.on('finish', function () {
+    onFinish(this.buf);
+  });
 }
 inherits(Collector, stream.Writable);
 
 Collector.prototype._write = function (chunk, cb) {
   this.buf += chunk;
-  console.log('got buf now', this.buf);
   cb(null);
 };
 
-//test("all at once", function (t) {
-  // TODO: make a readable stream to read from
+test("sample buffer", function (t) {
+  t.plan(1)
+  // dont end in newline so we know _flush works
   var buff = "hi there miss\nnice to meat you\n!!";
+  var onFinish = function (res) {
+    t.equal(buff.split('\n').join(''), res, "output == split input");
+  };
 
-  var pmp = new Pumper(buff);
-  var clct = new Collector(function () {
-    console.log('done!');
-  });
-  //pmp.pipe(process.stdout)
-  pmp.pipe(splitter()).pipe(clct);
+  var pump = new Pumper(buff);
+  var collect = new Collector(onFinish);
 
-  //pmp.pipe(splitter()).pipe(process.stdout);
-  // TODO: make a writable stream to pipe to (that collects)
-//  t.ok(true, 'woo');
-//  t.end();
-//});
+  pump.pipe(splitter()).pipe(collect);
+});
